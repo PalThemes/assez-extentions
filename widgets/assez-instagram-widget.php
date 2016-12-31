76ef13810a956db49724b63cbf50e0af41dfc6b5
PalThemes <deo.hoshi@gmail.com>
2016-12-31 21:02:56 +0700

    Add files via upload

--- widgets/assez-instagram-widget.php
+++ widgets/assez-instagram-widget.php
@@ -65,7 +65,7 @@ class assez_instagram_widget extends WP_Widget {
 					<li class="'. esc_attr( $liclass ) .'">
 						<a href="'. esc_url( $item['link'] ) .'" target="'. esc_attr( $target ) .'"  class="'. esc_attr( $aclass ) .'">
 							' . $likes_comments . '
-							<img src="'. esc_url( $item[$size] ) .'"  alt="'. esc_attr( $item['description'] ) .'" title="'. esc_attr( $item['description'] ).'"  class="'. esc_attr( $imgclass ) .'"/>
+							<img src="" data-src="'. esc_url( $item[$size] ) .'" alt="" title="'. esc_attr( $item['description'] ).'"  class="'. esc_attr( $imgclass ) .'"/>
 						</a>
 					</li>';
 				}
